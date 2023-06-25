8713e6d42b634870b9cb8c96015a50fffe301d1c
rhf007 <rahofahazem512@gmail.com>
2023-06-25 11:26:06 -0700

    2

---
+++ pr1/get_spec_func.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * get_spec_func - get function for each specifier
+ * @c: specifier
+ * Return: pointer to suitable function
+ */
+int (*get_spec_func(char c))(va_list args)
+{
+	int i;
+	spec specifier[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"d", print_num},
+		{"i", print_num},
+		{"o", print_num},
+		{"b", print_num},
+		{"x", print_num},
+		{"X", print_num},
+		{"%", print_%},
+		{NULL, NULL}
+	};
+
+	for (i = 0; spec[i].spec != '\0'; i++)
+	{
+		if (spec[i].spec[0] == c)
+		{
+			return(spec[i].f);
+		}
+	}
+
+	return (NULL);
+}
