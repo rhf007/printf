215d53b37fd7b92326a7ab0b70f4c9ce084f6cfc
Islam <islamabdoulradi@gmail.com>
2023-06-25 10:12:39 +0300

    hello

---
+++ _printf.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _printf - It will act like printf when give it s c % specifier
+ * @format: specifies the necessary operations
+ * Return: printed characters length
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, len = 0;
+
+	va_list args;
+
+	va_start(args, format);
+
+	while (format != NULL && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			len += checkSpecifier(format[i + 1], args, len);
+			i++;
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			len++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (len);
+}
