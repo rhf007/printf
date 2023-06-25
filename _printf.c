14cb124d6cfd52c2453f4a052d445a696d9743b2
rhf007 <rahofahazem512@gmail.com>
2023-06-24 23:17:31 -0700

    Update _printf.c

---
+++ _printf.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * _printf - It will act like printf when give it s c % specifier
+ * @format: specifies the necessary operations
+ * Return: printed characters length
+ */
+
