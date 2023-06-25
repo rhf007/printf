1a15d24bb91330da81b11292b49f84c39e24160e
rhf007 <rahofahazem512@gmail.com>
2023-06-24 23:19:49 -0700

    Update _printf.c 2

--- _printf.c
+++ _printf.c
@@ -5,4 +5,11 @@
  * @format: specifies the necessary operations
  * Return: printed characters length
  */
+int _printf(const char *format, ...)
+{
+	int i = 0. len = 0;
 
+	va_list args;
+
+	va_start(args, format);
+}
